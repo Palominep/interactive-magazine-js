dd2d45533e7c3e4e3a688060d68c38489e96a3d1
Levels Teen Center <levelsteencenter@Levelss-MacBook-Pro.local>
2019-03-22 16:54:45 -0400

    MOVED JAVA FILE

--- myJS.file.js
+++ myJS.file.js
@@ -3,3 +3,7 @@ document.getElementById("demo").innerHTML = "This is my new text";
 }
 
 document.getElementById("demo").style.color = "cornflowerblue";
+
+function badNameFunction(){
+document.getElementById("demo").innerHTML = "Anime Fandom United";
+}
--- myJSfile.js
+++
@@ -1,3 +0,0 @@
-function badNameFunction(){
-document.getElementById("demo").innerHTML = "Anime Fandom United";
-}
